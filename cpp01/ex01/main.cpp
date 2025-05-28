80d0f9010b706f99ef80a32dd592b9fd6a8f72dd
wsl <amsaafisalah2@gmail.com>
2025-05-28 17:51:13 +0100

    ex01

--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -3,9 +3,10 @@
 int main()
 {
     int i = 0;
-    //Zombie one("Bob");
+    zombie one("yeger");
     zombie *horde = zombieHorde(9, "eiren");
 
+    one.announce();
     for (i = 0; i < 8; i++)
     {
         horde[i].announce();
